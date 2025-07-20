4fa7170ec6ee20a8363ac942a042f75fe03568d1
aishwaryac808 <aishwaryac808@gmail.com>
2025-07-20 17:14:27 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,40 +1,21 @@
-// 1
 const urlInput = document.getElementById("urlInput");
-// 2
 const resultDiv = document.getElementById("result");
-// 3
+
 document.getElementById("roastBtn").addEventListener("click", function () {
-// 4
   const url = urlInput.value;
-// 5
+
   fetch("/api/roast", {
-// 6
     method: "POST",
-// 7
     headers: {
-// 8
       "Content-Type": "application/json",
-// 9
     },
-// 10
     body: JSON.stringify({ url: url }),
-// 11
-  })
-// 12
-    .then((response) => response.json())
-// 13
+  }).then((response) => response.json())
     .then((data) => {
-// 14
       resultDiv.innerText = data.roast;
-// 15
     })
-// 16
     .catch((error) => {
-// 17
       resultDiv.innerText = "Oops! Something went wrong.";
-// 18
       console.error("Roast API error:", error);
-// 19
     });
-// 20
 });
